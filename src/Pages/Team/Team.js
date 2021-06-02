54511830aefdb9f3532a59bee0fbbb01e4b7a050
Adam Norton <adam@adamnorton.com>
2021-06-01 23:51:53 -0500

    removed Erica from site

--- src/Pages/Team/Team.js
+++ src/Pages/Team/Team.js
@@ -143,7 +143,6 @@ class Team extends Component {
             <Gabriela />
             <Rachel />
             <Kacye />
-            <Erica />
             <Andrew />
             <Brooke />
             <Jazmin />
